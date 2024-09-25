52d1108eeb80cacf2b3ccdc8b43bb0ac25087307
Александр Ляшенко <sanko.lialko14@gmail.com>
2024-09-25 17:17:56 +0300

    f

--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import gitHubLight from '../../assets/github-light.svg'
 import gitHubDark from '../../assets/github-dark.svg'
 import linkedinLight from '../../assets/linkedin-light.svg'
 import linkedinDark from '../../assets/linkedin-dark.svg'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/CV.pdf'
 import { useTheme } from '../../common/ThemeContext'
 
 const Hero = () => {
