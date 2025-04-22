d0ad828902a25f0c161c46bf364dc5a73de27684
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-04-22 16:04:33 +0300

    c

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,15 +1,13 @@
-
-
-const ProjectCard = ({ src, link, h3, p }) => {
+const ProjectCard = ({ src, link, h3, links }) => {
   return (
-    <a href={link} target='black'>
-            <img className='hover'
-            src={src} 
-            alt={`${h3} Logo`} />
-            <h3>{h3}</h3>
-            <p>{p}</p>
+      <a href={link} target="black">
+        <img className="hover" src={src} alt={`${h3} Logo`} />
+        <h3>{h3}</h3>
+        <a href={links} target="black">
+          <p>Code</p>
         </a>
-  )
-}
+      </a>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/section/Projects/Projects.jsx
+++ src/section/Projects/Projects.jsx
@@ -19,55 +19,64 @@ const Projects = () => {
           src={game}
           link="https://alexliashenko19.github.io/2048-Game/"
           h3="2048 Game"
-          p="2048 Game"
+          a="2048 Game"
+          links="https://github.com/AlexLiashenko19/2048-Game"
         />
         <ProjectCard
           src={todo}
           link="https://alexliashenko19.github.io/Todo-App/"
           h3="Todo App App"
           p="Todo App App"
+          links="https://github.com/AlexLiashenko19/Todo-App"
         />
         <ProjectCard
           src={twitti}
           link="https://twitti.onrender.com/profile/Alex"
           h3="Social Media App"
           p="Social Media App"
+          links="https://github.com/AlexLiashenko19/Twitti"
         />
         <ProjectCard
           src={aqua}
           link="https://aqua-track-project.netlify.app/"
           h3="Aqua Track"
           p="Aqua Track"
+          links="https://github.com/BurglachOK/aqua-track-project-back"
         />
         <ProjectCard
           src={cinemania}
           link="https://andre29839.github.io/error-404_team_name_not_found/"
           h3="Cinemania"
           p="Cinema App"
+          links="https://github.com/Andre29839/error-404_team_name_not_found"
         />
         <ProjectCard
           src={chat}
           link="https://chat-c2gt.onrender.com"
           h3="Chat-App"
           p="Chat-App"
+          links="https://github.com/AlexLiashenko19/Chat"
         />
-        <ProjectCard 
-        src={crm}
-        link="https://crm-psi-seven.vercel.app/dashboard"
-        h3="Crm"
-        p="Crm App"
+        <ProjectCard
+          src={crm}
+          link="https://crm-psi-seven.vercel.app/dashboard"
+          h3="Crm"
+          p="Crm App"
+          links="https://github.com/AlexLiashenko19/CRM"
         />
         <ProjectCard
           src={music}
           link="https://alexliashenko19.github.io/Landing-Page/"
-          h3="Social Media App"
-          p="Social Media App"
+          h3="SoundNest  App"
+          p="SoundNest  App"
+          links="https://github.com/AlexLiashenko19/Landing-Page"
         />
         <ProjectCard
           src={fitnes}
           link="https://fitness-application-azure.vercel.app/"
           h3="Fitnes App"
           p="Fitnes App"
+          links="https://github.com/AlexLiashenko19/Fitness-Application"
         />
       </div>
     </section>
